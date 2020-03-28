314669ea8d18981a44fbc61e5fed9c73b327720d
elrhomari.younes <elrhomari.younes@gmail.com>
2020-03-28 09:06:24 +0100

    fix
    local_test use http

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ Vue.use(VueGoogleMaps, {
 });
 Vue.use(Vuetify)
 
-axios.defaults.baseURL='https://docketu.iutnc.univ-lorraine.fr:35043/'
+axios.defaults.baseURL='http://docketu.iutnc.univ-lorraine.fr:35080/'
 
 
 Vue.config.productionTip = false
